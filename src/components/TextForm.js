71f413bd92896d9bf5fb1d2a86feec6a3cc44d47
inzz <inzamampawaskar10@gmail.com>
2022-04-05 20:45:29 +0530

    added clear all function and change fonts function

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -39,14 +39,29 @@ export default function TextForm(props) {
       setText(newText);
     }
 
-    const toLowCase = ()=>{
+  const toLowCase = ()=>{
       let newText = text.toLowerCase();
       setText(newText);
     }
 
-    const onChangeFunc = (event)=>{
+  const onChangeFunc = (event)=>{
       setText(event.target.value);
     }
+
+  const clearAll = ()=>{
+      let newText = '';
+      setText(newText);
+    }
+
+  const toBold = ()=>{
+    let preview = document.getElementById('preview');
+    preview.innerHTML = `<b>${text}</b>`;
+  }
+
+  const toItalic = ()=>{
+    let preview = document.getElementById('preview');
+    preview.innerHTML = `<i>${text}</i>`;
+  }
 
   return (
     <>
@@ -56,13 +71,24 @@ export default function TextForm(props) {
       <div id="alert"></div>
       <button className="btn btn-primary my-3 mx-3" onClick={toUpCase}>convert to UPPERCASE</button>
       <button className="btn btn-primary my-3 mx-3" onClick={toLowCase}>CONVERT to lowercase</button>
+      <button className="btn btn-primary my-3 mx-3" onClick={clearAll}>Clear text</button>
+    <div className="dropdown mx-3">
+      <a className="btn btn-secondary dropdown-toggle" href="/" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+        Change font style
+      </a>
+
+      <ul className="dropdown-menu" id="fonts" aria-labelledby="dropdownMenuLink">
+        <li className="dropdown-item" onClick={toBold}><b>BOLD</b></li>
+        <li className="dropdown-item" onClick={toItalic}><i>ITALIC</i></li>
+      </ul>
     </div>
+   </div>
     <div className="container">
       <h2>Your text summary</h2>
       <p>{wordsLen(text)} words and {lettersLen(text).char} characters and {lettersLen(text).emptySpace} spaces.</p>
       <p>{0.125 * wordsLen(text)} minutes will take to read this.</p>
       <h3>Preview</h3>
-      <p>{text}</p>
+      <p id="preview" >{text}</p>
     </div>
     </>
   )
