9289fb027978df0c5f91169363636efee155465f
inzz <inzamampawaskar10@gmail.com>
2022-04-10 20:30:43 +0530

    Updates

---
+++ precache-manifest.d52ef518ab47a00a8876f1f6a25feff1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "751e4ddeb03665b47330",
+    "url": "/TextUtils-React/static/css/main.4158d0ab.chunk.css"
+  },
+  {
+    "revision": "751e4ddeb03665b47330",
+    "url": "/TextUtils-React/static/js/main.751e4dde.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/TextUtils-React/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "9f467190b4df4fe2318e",
+    "url": "/TextUtils-React/static/js/2.9f467190.chunk.js"
+  },
+  {
+    "revision": "2499cddb344818b4039d",
+    "url": "/TextUtils-React/static/js/runtime~main.2499cddb.js"
+  },
+  {
+    "revision": "71ba4406a99af21b7636911c9338b294",
+    "url": "/TextUtils-React/index.html"
+  }
+];
